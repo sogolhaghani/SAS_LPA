587635c31cf2ae71a0fa17e65de5c3e3e60487e2
sogolhaghani <haghani.sogol@gmail.com>
2020-03-29 08:03:33 +0430

    clean

--- src/lfr_test.py
+++
@@ -1,9 +0,0 @@
-from networkx.algorithms.community import LFR_benchmark_graph
-
-n = 15000
-tau1 = 3
-tau2 = 1.5
-mu = 0.1
-G = LFR_benchmark_graph(n, tau1, tau2, mu, average_degree=5, min_community=20, seed=10)
-# communities = {frozenset(G.nodes[v]['community']) for v in G}
-print(G.nodes[1]['community'])
